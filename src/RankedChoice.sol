bd5916ab751d00468a4a71c4661b9740f9a1c666
mkolev12 <mkolev@baybridgedigital.com>
2024-09-26 13:21:53 +0300

    add audit comments through the code

--- src/RankedChoice.sol
+++ src/RankedChoice.sol
@@ -16,10 +16,11 @@ contract RankedChoice is EIP712 {
     /*//////////////////////////////////////////////////////////////
                            STORAGE VARIABLES
     //////////////////////////////////////////////////////////////*/
-    address private s_currentPresident;
-    uint256 private s_previousVoteEndTimeStamp;
-    uint256 private s_voteNumber;
-    uint256 private immutable i_presidentalDuration;
+    address private s_currentPresident; // e current president
+    uint256 private s_previousVoteEndTimeStamp; // e last time a president was selected
+    uint256 private s_voteNumber; // e the current vote number
+    uint256 private immutable i_presidentalDuration; // e presidental duration set always as 1460 days(4 years)
+    // @audit invalid TYPEHASH signiture -> should be `rankCandidates(address[])` since `rankCandidates` accepts an array of addresses
     bytes32 public constant TYPEHASH = keccak256("rankCandidates(uint256[])");
     uint256 private constant MAX_CANDIDATES = 10;
 
@@ -37,16 +38,19 @@ contract RankedChoice is EIP712 {
                              USER FUNCTIONS
     //////////////////////////////////////////////////////////////*/
     constructor(address[] memory voters) EIP712("RankedChoice", "1") {
-        VOTERS = voters;
-        i_presidentalDuration = 1460 days;
-        s_currentPresident = msg.sender;
-        s_voteNumber = 0;
+        VOTERS = voters; // set the voters (specified by the contract creator)
+        i_presidentalDuration = 1460 days; // 4 years
+        s_currentPresident = msg.sender; // current president is the contract creator
+        s_voteNumber = 0; // it's by default 0
     }
 
+    // @follow-up - seems ok
     function rankCandidates(address[] memory orderedCandidates) external {
         _rankCandidates(orderedCandidates, msg.sender);
     }
 
+    // @follow-up - seems ok
+    // q possible signature replay attack ???
     function rankCandidatesBySig(
         address[] memory orderedCandidates,
         bytes memory signature
@@ -54,10 +58,17 @@ contract RankedChoice is EIP712 {
         bytes32 structHash = keccak256(abi.encode(TYPEHASH, orderedCandidates));
         bytes32 hash = _hashTypedDataV4(structHash);
         address signer = ECDSA.recover(hash, signature);
+
+        // q possible signature replay attack ???
+        // @follow-up should not be a problem since we encode the orderedCandidates in the signiture so
+        // a malicious actor can replay the signature but he needs to pass same orderedCandidates for the same voter
         _rankCandidates(orderedCandidates, signer);
     }
 
+    // @audit-high A malicious voter can front-run this `selectPresident` function by seeing how other people ranked their candidates
+    // and then rank their candidates accordingly to win the election and then immediately call this function to select the president
     function selectPresident() external {
+        // e check if election time has passed
         if (
             block.timestamp - s_previousVoteEndTimeStamp <=
             i_presidentalDuration
@@ -68,14 +79,21 @@ contract RankedChoice is EIP712 {
         for (uint256 i = 0; i < VOTERS.length; i++) {
             address[] memory orderedCandidates = s_rankings[VOTERS[i]][
                 s_voteNumber
-            ];
+            ]; // e get candidates for each voter
             for (uint256 j = 0; j < orderedCandidates.length; j++) {
+                // @audit-gas - This is a nested loop, and could be expensive if the number of candidates is large
+                // @audit gas - can create a copy of s_candidateList and use it to store the candidates so it does not read from the storage every time
                 if (!_isInArray(s_candidateList, orderedCandidates[j])) {
                     s_candidateList.push(orderedCandidates[j]);
                 }
             }
         }
 
+        // e in the end we should have always 1 winner
+
+        // VOTER1 = [0x1, 0x2, 0x3, 0x4, 0x5]
+        // VOTER2 = [0x6, 0x7, 0x8, 0x1, 0x3]
+        // s_candidateList = [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8]
         address[] memory winnerList = _selectPresidentRecursive(
             s_candidateList,
             0
@@ -95,23 +113,30 @@ contract RankedChoice is EIP712 {
     /*//////////////////////////////////////////////////////////////
                            CONTRACT FUNCTIONS
     //////////////////////////////////////////////////////////////*/
+ 
+    // q how can we maniupate the winner ???
     function _selectPresidentRecursive(
-        address[] memory candidateList,
-        uint256 roundNumber
+        address[] memory candidateList, // [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8], let's say 0x1 is in multiple rounds
+        uint256 roundNumber // 0
     ) internal returns (address[] memory) {
         if (candidateList.length == 1) {
             return candidateList;
         }
 
+        // e VOTERS will be a list of owner specified users who are able to vote for candidates(max 10)
         // Tally up the picks
-        for (uint256 i = 0; i < VOTERS.length; i++) {
+        for (uint256 i = 0; i < VOTERS.length; i++) { // for example 3 VOTERS
             for (
                 uint256 j = 0;
+                // @audit-gas - we can use local variable to store the length of the rankings array
                 j < s_rankings[VOTERS[i]][s_voteNumber].length;
                 j++
             ) {
+                // e get the candidate from the rankings
                 address candidate = s_rankings[VOTERS[i]][s_voteNumber][j];
-                if (_isInArray(candidateList, candidate)) {
+                // e check if the candidate is in the candidate list
+                if (_isInArray(candidateList, candidate)) { // check if the candidate is in the candidate list
+                    // q `s_candidateVotesByRound` is not used anywhere else in the contract so why it is a storage variable
                     s_candidateVotesByRound[candidate][s_voteNumber][
                         roundNumber
                     ] += 1;
@@ -131,8 +156,8 @@ contract RankedChoice is EIP712 {
         for (uint256 i = 1; i < candidateList.length; i++) {
             uint256 votes = s_candidateVotesByRound[candidateList[i]][
                 s_voteNumber
-            ][roundNumber];
-            if (votes < fewestVotes) {
+            ][roundNumber]; // check candadata votes
+            if (votes < fewestVotes) { // check if the votes are less than the fewest votes
                 fewestVotes = votes;
                 fewestVotesCandidate = candidateList[i];
             }
@@ -156,22 +181,30 @@ contract RankedChoice is EIP712 {
         return _selectPresidentRecursive(newCandidateList, roundNumber + 1);
     }
 
+    // @follow-up - seems ok
     function _rankCandidates(
-        address[] memory orderedCandidates,
-        address voter
+        address[] memory orderedCandidates, // [0x01234, 0x05678, 0x0910113]
+        address voter // 0x12345
     ) internal {
         // Checks
-        if (orderedCandidates.length > MAX_CANDIDATES) {
+        // @audit We can pass in orderCardidates same address multiple times [0x1, 0x1, 0x1, ...MAX_CANDIDATES]
+        // @audit we can pass our own voter address as a candidate // [myAddress, 0x1, 0x2, 0x3]
+        if (orderedCandidates.length > MAX_CANDIDATES) { // e check if the number of candidates is greater than the max
             revert RankedChoice__InvalidInput();
         }
-        if (!_isInArray(VOTERS, voter)) {
+        if (!_isInArray(VOTERS, voter)) { // e check if the specified voter is in the list of voters
             revert RankedChoice__InvalidVoter();
         }
+
+        // if (!_isInArray(orderedCandidates, voter)) {
+        //     revert();
+        // }
 
         // Internal Effects
         s_rankings[voter][s_voteNumber] = orderedCandidates;
     }
 
+    // @follow-up - seems ok
     function _isInArray(
         address[] memory array,
         address someAddress
--- test/RankedChoiceTest.t.sol
+++ test/RankedChoiceTest.t.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {RankedChoice} from "src/RankedChoice.sol";
+import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract RankedChoiceTest is Test {
     address[] voters;
+    uint256[] voterPrivateKeys;
+
     address[] candidates;
 
     uint256 constant MAX_VOTERS = 100;
@@ -19,7 +22,10 @@ contract RankedChoiceTest is Test {
 
     function setUp() public {
         for (uint256 i = 0; i < MAX_VOTERS; i++) {
-            voters.push(address(uint160(i + VOTERS_ADDRESS_MODIFIER)));
+            uint256 voterPrivateKey = uint256(keccak256(abi.encodePacked("dummy mnemonic", uint256(1))));
+            address voter = vm.addr(voterPrivateKey);
+            voterPrivateKeys.push(voterPrivateKey);
+            voters.push(voter);
         }
         rankedChoice = new RankedChoice(voters);
 
@@ -109,4 +115,72 @@ contract RankedChoiceTest is Test {
         rankedChoice.selectPresident();
         assertEq(rankedChoice.getCurrentPresident(), candidates[1]);
     }
+
+    // function testNonVoterIsAbleToOverrideVotersCandidates() public {
+    //     orderedCandidates = [candidates[0], candidates[1], candidates[2]];
+    //     vm.prank(voters[0]);
+    //     rankedChoice.rankCandidates(orderedCandidates);
+
+    //     bytes32 domainSeparator = keccak256(abi.encode(
+    //         keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+    //         keccak256(bytes("RankedChoice")),  // Name of the contract
+    //         keccak256(bytes("1")),             // Version of the contract
+    //         block.chainid,                     // Chain ID (use vm.chainId() for Foundry)
+    //         address(rankedChoice)              // The address of the deployed contract
+    //     ));
+
+    //     bytes32 structHash = keccak256(abi.encode(rankedChoice.TYPEHASH(), orderedCandidates));
+    //     bytes32 hash =  MessageHashUtils.toTypedDataHash(domainSeparator, structHash); // Use the same hash method as in the contract
+    //     (uint8 v, bytes32 r, bytes32 s) = vm.sign(voterPrivateKeys[0], hash);
+    //     bytes memory signature = abi.encodePacked(r, s, v);
+
+
+    //     address attacker = makeAddr("attacker");
+    //     vm.startPrank(attacker);
+    //     // orderedCandidates = [candidates[3], candidates[1], candidates[4]];
+    //     rankedChoice.rankCandidatesBySig(orderedCandidates, signature);
+    //     vm.stopPrank();
+    // }
+
+function testFrontRunPresidentSelection() public {
+    orderedCandidates = [candidates[1], candidates[2], candidates[3]];
+    vm.prank(voters[0]);
+    rankedChoice.rankCandidates(orderedCandidates);
+
+    // Voter 1 ranks candidate[3], candidate[2], candidate[1]
+    orderedCandidates = [candidates[3], candidates[2], candidates[1]];
+    vm.prank(voters[1]);
+    rankedChoice.rankCandidates(orderedCandidates);
+
+    // Voter 2 ranks candidate[2], candidate[1], candidate[3]
+    orderedCandidates = [candidates[2], candidates[1], candidates[3]];
+    vm.prank(voters[2]);
+    rankedChoice.rankCandidates(orderedCandidates);
+
+    // --- FRONT-RUNNING BEGINS ---
+    // Front-runner (Voter 3) spends extra gas (simulated here) to ensure their transaction is mined before `selectPresident`
+    // We simulate this by placing their `rankCandidates` call immediately before `selectPresident`
+
+    // Front-runner logic: Rank candidates strategically to favor `candidate[0]`
+    orderedCandidates = [candidates[0], candidates[3], candidates[2]];
+
+    // Simulate spending extra gas by adding a loop before casting vote (artificial gas consumption)
+    uint256 fakeGasSpend = 0;
+    for (uint256 i = 0; i < 1000; i++) {
+        fakeGasSpend += i; // Simulate some gas-intensive operation
+    }
+
+    vm.prank(voters[3]); // Front-runner casts their manipulated vote
+    rankedChoice.rankCandidates(orderedCandidates);
+
+    // Move time forward to simulate end of the voting period
+    vm.warp(block.timestamp + rankedChoice.getDuration());
+
+    // Select the president
+    rankedChoice.selectPresident();
+
+    // Verify that the front-runner's candidate (candidate[0]) won due to strategic voting
+    console.log("Selected President: ", rankedChoice.getCurrentPresident());
+    assertEq(rankedChoice.getCurrentPresident(), candidates[0]); // candidate[0] is the manipulated winner
+}
 }
